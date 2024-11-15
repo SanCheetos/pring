d68c84038e5bd566bb268914d925477cee85ec52
SanCheetos <94627584+SanCheetos@users.noreply.github.com>
2024-11-15 18:42:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ model.overrides['agnostic_nms'] = False  # NMS class-agnostic
 model.overrides['max_det'] = 1000  # maximum number of detections per image
 
 # set image
-image = 'https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg'
+#image = 'https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg'
 
 # perform inference
-results = model.predict(image)
+#results = model.predict(image)
