c932ed6d382ddafe226338adba5732929b42f18d
SanCheetos <94627584+SanCheetos@users.noreply.github.com>
2024-11-15 18:48:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import streamlit as st
-st.text("hello")
 from ultralyticsplus import YOLO, render_result
 
 # load model
-model = YOLO('keremberke/yolov8n-hard-hat-detection')
+# model = YOLO('keremberke/yolov8n-hard-hat-detection')
 
-# set model parameters
-model.overrides['conf'] = 0.25  # NMS confidence threshold
-model.overrides['iou'] = 0.45  # NMS IoU threshold
-model.overrides['agnostic_nms'] = False  # NMS class-agnostic
-model.overrides['max_det'] = 1000  # maximum number of detections per image
+# # set model parameters
+# model.overrides['conf'] = 0.25  # NMS confidence threshold
+# model.overrides['iou'] = 0.45  # NMS IoU threshold
+# model.overrides['agnostic_nms'] = False  # NMS class-agnostic
+# model.overrides['max_det'] = 1000  # maximum number of detections per image
 
 # set image
 #image = 'https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg'
