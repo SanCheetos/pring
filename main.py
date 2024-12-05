1c00618fb5b6652993419088e824c916b9996c08
xsancheetosx@gmail.com <xsancheetosx@gmail.com>
2024-12-06 00:23:23 +0500

    ....

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ if (uploaded_files1 or uploaded_files2):
 # @app.get("/")
 # async def root():
 #     return {"message": "Hello World"}
-from tests import model_tests
-model_tests.similarImg()
-model_tests.noImage()
+# from tests import model_tests
+# model_tests.similarImg()
+# model_tests.noImage()
+#
