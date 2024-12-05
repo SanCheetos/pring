d8c02da4b75d1e8ccec2ad08d46067b2f4eb59d8
xsancheetosx@gmail.com <xsancheetosx@gmail.com>
2024-12-06 00:39:49 +0500

    ....

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -8,8 +8,8 @@ sys.path.append(parent_dir)
 from myModel import check_faces_similarity
 
 
-def similarImg():
+def test_similarImg():
     assert check_faces_similarity("face1.jpg", "face1.jpg")== 0.0
 
-def noImage():
+def test_noImage():
     assert check_faces_similarity("asdasdas", "face1.jpg") == "Один из файлов не существует"
