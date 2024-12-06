d98558e904e0f06ddd52e947b463ec84305e27f9
SanCheetos <94627584+SanCheetos@users.noreply.github.com>
2024-12-06 19:16:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from tensorflow.keras.applications.resnet50 import preprocess_input
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
-#from typing import Annotated
-#from fastapi import FastAPI, File, UploadFile
-#from io import BytesIO
+# from typing import Annotated
+# from fastapi import FastAPI, File, UploadFile
+# from io import BytesIO
 
 from myModel import check_faces_similarity
 
@@ -32,21 +32,22 @@ if (uploaded_files1 or uploaded_files2):
         if (uploaded_files1):
             st.header("Изображение 1")
             st.image(uploaded_files1)
-    
     with col4:
         if (uploaded_files2):
             st.header("Изображение 2")
             st.image(uploaded_files2)
-    
     if (uploaded_files1 and uploaded_files2):
-        st.text(f"distance: {check_faces_similarity(uploaded_files1, uploaded_files2)}")
+        st.text(f"distance: {check_faces_similarity(uploaded_files1, 
+            uploaded_files2)}")
 
 # app = FastAPI()
 # @app.post("/files/")
-# async def create_file(file1: Annotated[UploadFile, File(...)], file2: Annotated[UploadFile, File(...)]):
+# async def create_file(file1: Annotated[UploadFile, File(...)], 
+# file2: Annotated[UploadFile, File(...)]):
 #     uFile1 = await file1.read() 
 #     uFile2 = await file2.read() 
-#     return {"distance": check_faces_similarity(BytesIO(uFile1), BytesIO(uFile2))}
+#     return {"distance": check_faces_similarity(BytesIO(uFile1), 
+#BytesIO(uFile2))}
 
 # @app.get("/")
 # async def root():
